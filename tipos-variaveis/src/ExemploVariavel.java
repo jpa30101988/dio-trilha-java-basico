8ea9ef7c353ea3576c7bb1d38231bd32b2010c1b
janio.araujo <aerojanio@gmail.com>
2024-05-25 15:06:53 -0300

    commit exemplo variaveis

---
+++ tipos-variaveis/src/ExemploVariavel.java
@@ -0,0 +1,18 @@
+public class ExemploVariavel {
+
+   
+    public static void main(String[] args) {
+       
+    int numero = 5;
+     
+    numero = 10;
+
+    System.out.print(numero);
+            
+    double VALOR_DE_PI = 3.14;
+       
+    VALOR_DE_PI=3.15; //Esta linha vai apresentar erro de compilação!
+    }
+	
+
+}
