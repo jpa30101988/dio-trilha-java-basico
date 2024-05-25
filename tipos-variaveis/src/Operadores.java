43b27a3b2b2b863ab97eddbfddd2a3de1837bf9f
janio.araujo <aerojanio@gmail.com>
2024-05-25 16:33:09 -0300

    commit operadores java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,66 @@
+public class Operadores {
+    public static void main(String[] args) {
+ /**
+ * Atribuição
+ */
+
+String nome = "JANIO";
+System.out.println(nome);
+int idade = 22;
+System.out.println(idade);
+double peso = 94.5;
+System.out.println(peso);
+char sexo = 'M';
+System.out.println(sexo);
+boolean doadorOrgao = false;
+System.out.println(doadorOrgao);
+;
+
+/**
+ * Junçao de nomes
+ */
+
+String nomeCompleto = "LINGUAGEM" + "JAVA";
+System.out.println(nomeCompleto);
+
+/**
+ * Aritimetico
+ * 
+ */
+
+ //classe Operadores.java
+double soma = 10.5 + 15.7;
+System.out.println(soma);
+int subtração = 113 - 25;
+System.out.println(subtração);
+int multiplicacao = 20 * 7;
+System.out.println(multiplicacao);
+int divisao = 15 / 3;
+System.out.println(divisao);
+int modulo = 18 % 3;
+System.out.println(modulo);
+double resultado = (10 * 7) + (20/4);
+System.out.println(resultado);
+
+/**
+ * COncatenação
+ */
+
+String concatenacao ="?";
+
+concatenacao = 1+1+1+"1";
+System.out.println(concatenacao);
+concatenacao = 1+"1"+1+1;
+System.out.println(concatenacao);
+concatenacao = 1+"1"+1+"1";
+System.out.println(concatenacao);
+concatenacao = "1"+1+1+1;
+System.out.println(concatenacao);
+concatenacao = "1"+(1+1+1);
+System.out.println(concatenacao);
+
+
+
+    }
+    
+}
